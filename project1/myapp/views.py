fc811c7f51ce22c8ab9f38923c33306e5ad7de78
mhnayeem2000 <mhnayeem018460@gmail.com>
2024-07-09 09:15:43 +0600

    Load data from Backend

--- project1/myapp/views.py
+++ project1/myapp/views.py
@@ -2,4 +2,24 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'myapp/home.html')
+
+    data = [
+    {
+        "title": "Top 10 important tips on IT services & design",
+        "category" : "Technology",
+        "img" : "/static/blog-1.jpg",
+        
+    },
+    {
+        "title": "How our company works in different ways",
+        "category" : "Design",
+        "img" : "/static/blog-2.jpg",
+    },
+    {
+        "title": "How is technology working with new things?",
+        "category" : "Artificial",
+        "img" : "/static/blog-3.jpg",
+    }
+    ]
+
+    return render(request, 'myapp/home.html' , {"data" : data})
