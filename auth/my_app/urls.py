8cef3b7ae6813d54c5b55758f11df0b8abb25cde
mhnayeem2000 <mhnayeem018460@gmail.com>
2024-07-28 13:02:32 +0600

    Authentication

--- auth/my_app/urls.py
+++ auth/my_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home , name='homepage'),
     path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
     path('profile/', views.profile, name='profile'),
     path('register/', views.register , name='register'),
     path('user_create_success', views.user_create_success, name='user_create_success'),
--- auth/my_app/views.py
+++ auth/my_app/views.py
@@ -8,32 +8,44 @@ def home(request):
     return render(request, 'home.html')
 
 def user_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request= request , data = request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request,username = name, password = password)
-            if user is not None:
-                login(request,user)
-                return redirect('profile')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request= request , data = request.POST)
+            if form.is_valid():
+                name = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+                user = authenticate(request,username = name, password = password)
+                if user is not None:
+                    login(request,user)
+                    return redirect('profile')
+        else:
+            form = AuthenticationForm()               
+        return render(request, 'login.html', {'form': form})
     else:
-        form = AuthenticationForm()               
-    return render(request, 'login.html', {'form': form})
+        return redirect('profile')
 
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
 
 def profile(request):
-    return render(request, 'profile.html', {'user': request.user})
-
-def register(request):
-    if request.method == 'POST':
-        form = registrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
+    if request.user.is_authenticated:
+        return render(request, 'profile.html', {'user': request.user})
     else:
-        form = registrationForm()        
-    return render(request, 'register.html', {'form': form})
+        return redirect('login')
+def register(request):
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = registrationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('login')
+        else:
+            form = registrationForm()        
+        return render(request, 'register.html', {'form': form})
+    else:
+        return redirect('profile')
 
 def user_create_success(request):
     return render(request, 'user_succ.html')
